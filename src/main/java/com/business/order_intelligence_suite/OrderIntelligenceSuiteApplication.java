feaf60cf0e26c4b5c1f60c97204e48095d5b4b8b
nagpalmayank <mayank.nagpal.548@gmail.com>
2024-09-05 02:14:19 +0530

    Commit Day 1

--- src/main/java/com/business/order_intelligence_suite/OrderIntelligenceSuiteApplication.java
+++ src/main/java/com/business/order_intelligence_suite/OrderIntelligenceSuiteApplication.java
@@ -3,7 +3,7 @@ package com.business.order_intelligence_suite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class OrderIntelligenceSuiteApplication {
 
 	public static void main(String[] args) {
